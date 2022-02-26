f0ac30dbdf9b84586b778dfebe4c8bb9342c57b0
Sarvesh Patil <sarveshspatil111@gmail.com>
2022-02-26 22:36:28 +0530

    :art: one-dark

---
+++ axyl-dwmblocks/blocks.h
@@ -0,0 +1,10 @@
+static const Block blocks[] = {
+	// {"Icon", "Command", "update interval", "update signal"}
+	//{"", "/opt/axyl-dwmblocks/scripts/packages",30,0},
+	{"", "/opt/axyl-dwmblocks/scripts/volume",1,0},
+	{"", "/opt/axyl-dwmblocks/scripts/brightness",1,0},
+	{"", "/opt/axyl-dwmblocks/scripts/battery",30,0},
+	{"", "/opt/axyl-dwmblocks/scripts/memory",3,0},
+	{"", "/opt/axyl-dwmblocks/scripts/clock",10,0},
+};
+static char delim = ' ';
